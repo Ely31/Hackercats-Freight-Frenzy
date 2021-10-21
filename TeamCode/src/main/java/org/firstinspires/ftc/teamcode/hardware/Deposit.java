8ceae3004afe7da10822b993c065221ce1077128
Ely31 <78882466+Ely31@users.noreply.github.com>
2021-10-21 13:10:48 -0500

    most of the .dump code is contained in the Deposit class now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Deposit.java
@@ -1,37 +1,49 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Config
 public class Deposit {
 
     private Servo pusher;
     private Servo door;
 
-    //time to wait before we close the door and reset
-    // the pusher after the freight has fallen out
-    private ElapsedTime falltime;
+    double pusherhome; // The retracted position of the pusher
+    double doorhome; // The closed position of the door
 
     //delay between opening the door and pushing freight out
-    private ElapsedTime pusherlag;
+    public static long pusherlag = 50;
+
+    //time to wait before we close the door and reset
+    // the pusher after the freight has fallen out
+    public static long falltime = 300;
 
     public void init(HardwareMap hwmap) {
         pusher = hwmap.get(Servo.class, "pusher");
         door = hwmap.get(Servo.class, "door");
 
-        double pusherhome = 0.05;
-        double doorhome = 0.1;
+        pusherhome = 0.05;
+        doorhome = 0.1;
 
         pusher.setPosition(pusherhome);
         door.setPosition(doorhome);
-
-        falltime.reset();
-        pusherlag.reset();
     }
 
-    public void dump(){
-        door.setPosition(0.6);
-        pusher.setPosition(0.45);
+    public void dump(ElapsedTime time){
+        if (time.milliseconds() < pusherlag) door.setPosition(0.6); // Open the door if the timer is less than pusherlag
+        if (time.milliseconds() > pusherlag && time.milliseconds() < pusherlag+falltime) pusher.setPosition(0.45); // Push freight out once pusherlag is over
+        if (time.milliseconds() > pusherlag+falltime) { // Reset deposit after falltime is over
+            pusher.setPosition(pusherhome);
+            door.setPosition(doorhome);
+        }
+
+    }
+
+    public void reset(){
+        pusher.setPosition(pusherhome);
+        door.setPosition(doorhome);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testFullTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testFullTeleop.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.hardware.Deposit;
 import org.firstinspires.ftc.teamcode.hardware.Intake;
@@ -13,7 +15,9 @@ public class testFullTeleop extends LinearOpMode {
 
     TeleopDrive drive = new TeleopDrive();
     Intake intake = new Intake();
+    Deposit deposit = new Deposit();
 
+    ElapsedTime dumptime = new ElapsedTime();
 
     boolean prevA;
     boolean intakeOn;
@@ -23,9 +27,12 @@ public class testFullTeleop extends LinearOpMode {
         // Init
         drive.init(hardwareMap);
         intake.init(hardwareMap);
+        deposit.init(hardwareMap);
 
         intakeOn = false;
         prevA = false;
+
+        dumptime.reset();
 
         boolean intakeIsReverseing = false;
 
@@ -39,7 +46,7 @@ public class testFullTeleop extends LinearOpMode {
             //mecdrive
             drive.drive(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_x,gamepad1.right_trigger);
 
-            //intake control
+            //intake control (this code is a nightmare)
             if (gamepad1.b) {
                 intake.reverse();
                 intakeIsReverseing = true;
@@ -56,6 +63,12 @@ public class testFullTeleop extends LinearOpMode {
             }
             prevA = gamepad1.a;
 
+
+            // deposit
+            if (gamepad1.left_bumper){
+                dumptime.reset();
+            }
+            deposit.dump(dumptime);
         }
     }
 }
