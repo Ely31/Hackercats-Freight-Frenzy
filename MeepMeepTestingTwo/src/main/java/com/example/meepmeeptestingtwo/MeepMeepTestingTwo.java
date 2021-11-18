4dad03019d23a131fa7225e23b554af3ae01fa8b
Ely31 <78882466+Ely31@users.noreply.github.com>
2021-11-17 21:43:08 -0600

    carouselsideauto created

--- MeepMeepTestingTwo/src/main/java/com/example/meepmeeptestingtwo/MeepMeepTestingTwo.java
+++ MeepMeepTestingTwo/src/main/java/com/example/meepmeeptestingtwo/MeepMeepTestingTwo.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptestingtwo;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -10,17 +11,19 @@ public class MeepMeepTestingTwo {
 
     static double originToWall = 141.0/2.0; // I guess the field is actually 141 inches wide
     static double wallDistance = originToWall - 6.5; // Center of bot is 7in from wall
+    static double carouselXCoordinate = -50;
+    static double carouselYCoordinate = -60;
 
     static Pose2d startPos = new Pose2d(-35,-(originToWall-9), Math.toRadians(-90));
 
-    static Pose2d preloadDepositPos = new Pose2d(-16,-45,Math.toRadians(260));
+    static Pose2d preloadDepositPos = new Pose2d(-16,-45,Math.toRadians(250));
 
     public static void main(String[] args) {
         // Declare a MeepMeep instance
         // With a field size of 700 pixels at 50 fps
         MeepMeep meepMeep = new MeepMeep(740,60);
 
-        RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep) // Create a bot
                 .setDimensions(11.6,18) // Width of 11.6 to match our thin bot
                 .setConstraints(55, 30, Math.toRadians(167), Math.toRadians(167), 11)
 
@@ -29,10 +32,13 @@ public class MeepMeepTestingTwo {
                         drive.trajectorySequenceBuilder(startPos)
                                 .lineToSplineHeading(preloadDepositPos)
                                 .waitSeconds(1)
-                                .splineToSplineHeading(new Pose2d(20,-wallDistance,Math.toRadians(0)),Math.toRadians(0))
-                                .lineToSplineHeading(new Pose2d(40,-wallDistance, Math.toRadians(0)))
+                                .lineToSplineHeading(new Pose2d(carouselXCoordinate,carouselYCoordinate,Math.toRadians(0))) // Go to carousel
+                                .waitSeconds(3) // Spin carousel
+                                .lineTo(new Vector2d(carouselXCoordinate+7,carouselYCoordinate)) // Back off carousel
+                                .lineToSplineHeading(new Pose2d(-60,-35,Math.toRadians(0)))
                                 .build()
                 );
+
         meepMeep
                 .setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
                 // Set theme
